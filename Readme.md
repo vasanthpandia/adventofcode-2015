f115e110cad23a78b6da7690f0898e3f16a3fe89
Vasanth Pandiarajan <itsmevasanth@hotmail.com>
2019-08-05 17:18:52 -0500

    Day 1 Completed

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# ReadMe
+
+## Advent of Code - 2015
+
+Refer to the site for details about Advent of Code - [Advent of Code 2015](https://adventofcode.com/2015/)
---
+++ day1.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"bufio"
+)
+
+func main() {
+	floorNumber()
+}
+
+func floorNumber() {
+	f, _ := os.Open("day1.txt")
+	reader := bufio.NewReader(f)
+	content, _ := ioutil.ReadAll(reader)
+
+	currentFloor := 0
+	var charPosition int
+	fmt.Println("CharPosition : ", charPosition)
+
+  for i := 0; i < len(content); i++ {
+		if content[i] == 40 {
+			currentFloor++
+		} else if content[i] == 41 {
+			currentFloor--
+		}
+
+		if currentFloor == -1 && charPosition == 0 {
+			charPosition = i + 1
+		}
+	}
+
+	fmt.Println("Final floor is : ", currentFloor)
+	fmt.Println("CharPosition : ", charPosition)
+}
