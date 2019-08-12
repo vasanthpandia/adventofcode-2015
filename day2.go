82a318b881986b3a822d8957683a80b7f0fc7b22
Vasanth Pandiarajan <itsmevasanth@hotmail.com>
2019-08-11 21:10:49 -0500

    Day2 - Part2 Completed

--- day2.go
+++ day2.go
@@ -20,6 +20,7 @@ func sheetArea() {
 	reader := bufio.NewReader(file)
 
 	var finalArea float64
+	var finalRibbonLength float64
 
 	for {
 		line, err := reader.ReadString('\n')
@@ -44,7 +45,44 @@ func sheetArea() {
 		fmt.Println(side1, side2, side3)
 
 		finalArea += findArea(side1, side2, side3)
+		finalRibbonLength += ribbonLength(side1, side2, side3)
 	}
 
-	fmt.Printf("Final Area : %f", finalArea)
+	fmt.Printf("Final Area : %f\n", finalArea)
+	fmt.Printf("Final Ribbon Length : %f\n", finalRibbonLength)
+}
+
+func findArea(side1 float64, side2 float64, side3 float64) float64 {
+	area1 := side1 * side2
+	area2 := side2 * side3
+	area3 := side1 * side3
+
+	area := 2 * (area1 + area2 + area3)
+
+	var smallest float64
+
+	if area1<=area2 && area1<=area3 {
+		smallest = area1
+	}
+	if area2<=area1 && area2<=area3 {
+		smallest = area2
+	}
+	if area3<=area1 && area3<=area2 {
+		smallest = area3
+	}
+
+	return (area + smallest)
+}
+
+func ribbonLength(side1 float64, side2 float64, side3 float64) float64 {
+	dims := []float64{side1, side2, side3}
+	sort.Float64s(dims)
+
+	smallPeri := 2 * (dims[0] + dims[1])
+
+	return smallPeri + volume(side1, side2, side3)
+}
+
+func volume(side1 float64, side2 float64, side3 float64) float64 {
+	return side1 * side2 * side3
 }
